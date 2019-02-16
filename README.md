9c99070600775e8d8bcadfbf48ef639ef57c565f
Marcin S <scatman@bu.edu>
2019-02-16 18:13:05 +0100

    make initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# org-recur
+
+Recurring org-mode tasks.
+
+## Screenshot
+
+![Screenshot](screenshot.png)
+
+## About
+
+This package extends org-mode and org-agenda with support for defining recurring tasks.
+
+I initially wrote this package for myself, because I've found that simple task management systems are the most effective for me. With this package I can just press `d` in the org-agenda to easily reschedule a recurring chore -- neat! I hope that others happen to find this useful as well.
+
+## Usage
+
+By adding some simple syntax to a task heading you can control how the task should recur. Examples:
+
++ `|+2|`: Recur every other day.
++ `|+w|`: Recur every week.
++ `|1|`: Recur on the first of every month.
++ `|Thu|`: Recur every Thursday.
++ `|Sun,Sat|`: Recur every Sunday and Saturday.
++ `|Wkdy|`: Recur every weekday.
+
+You can use the provided command `org-recur-finish` to reschedule tasks based on their recurrence pattern.
+
+## Installing
+
+Make sure you have set up [MELPA](http://melpa.milkbox.net/#/getting-started) and run:
+
+```
+M-x package-install RET org-recur RET
+```
+
+Or, if you have [use-package](https://github.com/jwiegley/use-package):
+
+```elisp
+(use-package org-recur
+  :demand t)
+```
+
+## Recommended Configuration
+
+The following `use-package` configuration sets the suggested keybindings (`C-c d`, as well as `d` in `org-agenda-mode`). It also enables `org-recur-finish` to act on headings without org-recur syntax, marking them done and archiving them.
+
+```elisp
+(use-package org-recur
+  :demand t
+  :bind ("C-c d" . org-recur-finish)
+
+  :config
+
+  ;; Rebind the 'd' key in org-agenda (default: `org-agenda-day-view').
+  (define-key org-agenda-mode-map (kbd "d") 'org-recur-finish)
+
+  (setq org-recur-finish-done t
+        org-recur-finish-archive t)
+  )
+```
+
+## Recommended org-mode settings
+
+Here are some org-mode settings that work well in conjunction with org-recur.
+
+Refresh the org-agenda whenever a task is rescheduled:
+
+```elisp
+;; Refresh org-agenda after rescheduling a task.
+(defun org-agenda-refresh ()
+  "Refresh all `org-agenda' buffers."
+  (dolist (buffer (buffer-list))
+    (with-current-buffer buffer
+      (when (derived-mode-p 'org-agenda-mode)
+        (org-agenda-maybe-redo)
+        ))))
+
+(defadvice org-schedule (after refresh-agenda activate)
+  "Refresh org-agenda."
+  (org-agenda-refresh))
+```
+
+Keep the task metadata clean:
+
+```elisp
+;; Log time a task was set to Done.
+(setq org-log-done (quote time))
+
+;; Don't log the time a task was rescheduled or redeadlined.
+(setq org-log-redeadline nil)
+(setq org-log-reschedule nil)
+```
+
+## Alternatives
+
+### Repeated tasks
+
+org-mode already supports ["repeated tasks"](https://orgmode.org/manual/Repeated-tasks.html), but it has some shortcomings:
+
++ Tasks need to have a TODO status set. I have a *lot* of recurring tasks and I don't want them all to be TODO.
++ You can't see how often a task recurs from the org-agenda view, you only see the task's headline and not its `SCHEDULED`/`DEADLINE` timestamps.
++ Repeated tasks require the `.+` syntax to shift the date based on today, which is what I almost always want. If I want to schedule a `+2` task to tomorrow I can do that manually, but I still want it to be clear that the task should recur every two days.
+
+org-recur is also simpler. I want to think as little as possible when I organize my time -- this helps keep my personal time management frictionless.
---
+++ org-recur.el
@@ -0,0 +1,212 @@
+;;; org-recur.el --- Recurring org-mode tasks. -*- lexical-binding: t; -*-
+;;
+;; Filename:    org-recur.el
+;; Description: Recurring org-mode tasks.
+;; Author:      Marcin Swieczkowski <marcin.swieczkowski@gmail.com>
+;; Created:     Fri Feb 15 2017
+;; Version:     0.0.1
+;; Package-Requires: ((emacs "24") (org "9.0"))
+;; URL:         https://github.com/m-cat/org-recur
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;; About:
+;;
+;; See README.md.
+;;
+;; Installing:
+;;
+;; See README.md for the most up-to-date installation instructions.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+;; Requirements
+
+(require 'org)
+(require 'org-agenda)
+
+;; Customize group
+
+(defgroup org-recur nil
+  "Recurring org-mode tasks."
+  :group 'faces)
+
+(defface org-recur
+  '((t :inherit org-tag))
+  "Face to highlight org-recur dates."
+  :group 'org-recur)
+
+(defcustom org-recur-finish-done nil
+  "Non-nil if calling `org-recur-finish' on a task without \
+org-recur syntax should mark it as DONE."
+  :type 'boolean
+  :group 'org-recur)
+
+(defcustom org-recur-finish-archive nil
+  "Non-nil if calling `org-recur-finish' on a task without \
+org-recur syntax should archive it."
+  :type 'boolean
+  :group 'org-recur)
+
+;; Internals
+
+(defconst org-recur--regexp "|\\([^|]*\\)|")
+
+(defconst org-recur--full-keywords `((,org-recur--regexp 0 'org-recur t)))
+
+(defvar org-recur--buffer-keywords nil)
+
+(defun org-recur--turn-on ()
+  "Set up `org-recur-mode'."
+  (let ((keywords org-recur--full-keywords))
+    (set (make-local-variable 'org-recur--buffer-keywords) keywords)
+    (font-lock-add-keywords nil keywords t)))
+(defun org-recur--turn-off ()
+  "Tear down `org-recur-mode'."
+  (when org-recur--buffer-keywords
+    (font-lock-remove-keywords nil org-recur--buffer-keywords)
+    (kill-local-variable 'org-recur--buffer-keywords)))
+
+(defun org-recur--date-string-to-time (org-date-string)
+  "Convert ORG-DATE-STRING to a time value."
+  (let ((time (org-read-date-analyze org-date-string nil nil)))
+    (encode-time 0 0 0 (nth 3 time) (nth 4 time) (nth 5 time)))
+  )
+(defun org-recur--date-less-p (D1 D2)
+  "Return non-nil if date string D1 is earlier than date string D2.
+A nil value is always considered greater than any date string.
+See ‘org-read-date’ for the various forms of a date string."
+  (if (or (not D2) (string= "" D2))
+      t
+    (if (or (not D1) (string= "" D1))
+        nil
+      (time-less-p (org-recur--date-string-to-time D1)
+                   (org-recur--date-string-to-time D2)))))
+
+;; (assert (org-date-less-p "+1" "+2"))
+;; (assert (not (org-date-less-p "+3" "+2")))
+;; (assert (org-date-less-p "+1" nil))
+;; (assert (org-date-less-p "+1" ""))
+
+(defconst org-recur--weekday "wkdy")
+(defconst org-recur--weekday-recurrence "mon,tue,wed,thu,fri")
+
+(defun org-recur--get-next-date (heading)
+  "Gets the next date to schedule to based on HEADING, or NIL \
+if no recurrence found."
+  (cond ((string-match org-recur--regexp heading)
+         (let* (
+                ;; Get the recurrence string.
+                ;; Replace any occurrence of "wkdy" (case-insensitive).
+                (recurrence (replace-regexp-in-string
+                             org-recur--weekday org-recur--weekday-recurrence
+                             (match-string 1 heading)))
+                ;; Split the recurrence as it may contain multiple options.
+                (options (split-string recurrence ","))
+                ;; Get the earliest option.
+                (next-date
+                 (let (value)
+                   (dolist (elt options value)
+                     (setq value (if (org-recur--date-less-p elt value) elt value))
+                     )))
+                )
+           next-date
+           ))
+        (t nil)
+        ))
+
+;; Autoloads
+
+;;;###autoload
+(defun org-recur-finish ()
+  "Reschedule an `org-mode' task containing an org-recur date string \
+according to the date. The org-recur syntax is '|DATE|', where
+DATE can be either an absolute date or a delta, so e.g. a task
+heading containing '|+2|' indicates to `org-recur-finish' to
+reschedule the task to two days from now.
+
+All date strings supported by `org-read-date' are available, in
+addition to 'wkdy', which schedules the task to the next weekday,
+and the 'N1,N2,...' syntax, wherein the earliest date string
+among the set of N is selected. For example, '|Mon,Fri|'
+indicates that the task should recur every Monday and Friday.
+
+If the task does not contain org-recur syntax, then depending on
+the values of `org-recur-finish-done' and
+`org-recur-finish-archive' change the task status to DONE and/or
+archive it."
+  (interactive)
+  (let* ((is-agenda (string= "org-agenda-mode" major-mode))
+         (heading (if is-agenda
+                      ;; FIXME: Find a more robust way of getting the header
+                      ;; from org-agenda view? This approach seems sufficient so
+                      ;; far though.
+                      (buffer-substring-no-properties
+                       (line-beginning-position) (line-end-position))
+                    (substring-no-properties (org-get-heading))))
+         (next-date (org-recur--get-next-date heading)))
+    (cond (next-date
+           (if is-agenda
+               (org-agenda-schedule nil next-date)
+             (org-schedule nil next-date)))
+           (t
+            (when org-recur-finish-done
+              (if is-agenda
+                  (org-agenda-todo 'done)
+                (org-todo 'done)))
+            (when org-recur-finish-archive
+              (if is-agenda
+                  (org-agenda-archive)
+                (org-archive-subtree)))
+            ))))
+
+;;;###autoload
+(add-hook 'org-mode-hook 'org-recur-mode)
+;;;###autoload
+(add-hook 'org-agenda-mode-hook 'org-recur-mode)
+
+;;;###autoload
+(add-hook 'org-agenda-finalize-hook
+          (lambda ()
+            (highlight-regexp org-recur--regexp 'org-recur)))
+
+;;;###autoload
+(define-minor-mode org-recur-mode
+  "Highlight org-recur dates and add commands for rescheduling them.
+
+With a prefix argument ARG, enable org-recur mode if ARG is positive, and
+disable it otherwise. If called from Lisp, enable the mode if ARG is omitted or
+nil, and toggle it if ARG is `toggle'."
+  :init-value nil
+  :lighter ""
+  :keymap nil
+  (org-recur--turn-off)
+  (when org-recur-mode
+    (org-recur--turn-on))
+  (when font-lock-mode
+    (if (fboundp 'font-lock-flush)
+        (font-lock-flush)
+      (with-no-warnings (font-lock-fontify-buffer)))))
+
+(provide 'org-recur)
+;;; org-recur.el ends here
